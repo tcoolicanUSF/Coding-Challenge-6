33f74fb1442c49de6c21f99f7633d4b0fc3de92e
tcoolican <tcoolican@usf.edu>
2024-09-22 22:49:28 -0400

    Initialize employees with shift schedules

--- employee_shift_management.js
+++ employee_shift_management.js
@@ -1,1 +1,30 @@
+//Coding Challenge #6
+//Task 1: Create an Employees Array of Employee Objects
+const employees = [{
+      name: "Lamar Miller",
+      shifts: [
+        { day: "Monday", hours: 8 },
+        { day: "Thursday", hours: 6 },
+        { day: "Friday", hours: 7 }
+      ]},{
+      name: "Myles Gaskin",
+      shifts: [
+        { day: "Tuesday", hours: 9 },
+        { day: "Wednesday", hours: 8 },
+        { day: "Saturday", hours: 5 }
+      ]},{
+      name: "Jay Ajayi",
+      shifts: [
+        { day: "Monday", hours: 8 },
+        { day: "Wednesday", hours: 7 },
+        { day: "Sunday", hours: 8 }
+      ]},{
+      name: "Raheem Mostert",
+      shifts: [
+        { day: "Wednesday", hours: 9 },
+        { day: "Friday", hours: 7 },
+        { day: "Sunday", hours: 6 }
+      ]}];
+
+//Task 2: Create a Function to Display Employee Shift Details
 
